371d21a0f5dbcdf56de65a7c1b5335063aa54e6f
Futaku <futaku0811@gmail.com>
2022-05-01 21:48:40 +0900

    start

---
+++ Memo/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  Memo
+//
+//  Created by Takumi Fuzawa on 2022/05/01.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController{
+    override func viewDidLoad() {
+        print("Hello World")
+    }
+}
